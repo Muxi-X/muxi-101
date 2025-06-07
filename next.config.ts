2b93593d5df30111d7d6e6c39aa5d5b477d18b59
bingling-sama <bingling_sama@qq.com>
2025-06-07 18:51:38 +0800

    ✨ feat(theme): build up guide and blog theme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import nextra from "nextra";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withNextra = nextra({
+})
+
+const nextConfig: NextConfig = withNextra({
+});
 
 export default nextConfig;
---
+++ src/mdx-components.tsx
@@ -0,0 +1,15 @@
+import { useMDXComponents as getDocsThemeComponents } from 'nextra-theme-docs' // nextra-theme-blog or your custom theme
+import { useMDXComponents as getBlogThemeComponents } from 'nextra-theme-blog' // nextra-theme-blog or your custom theme
+
+// Get the default MDX components
+const docsThemeComponents = getDocsThemeComponents()
+const blogThemeComponents = getBlogThemeComponents()
+
+// Merge components
+export function useMDXComponents(components: Record<string, React.ComponentType<any>>) {
+  return {
+    ...docsThemeComponents,
+    ...blogThemeComponents,
+    ...components
+  }
+}
