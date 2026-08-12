1461d90527ae7f4ace7e3cc18cf6b0bfd159a49a
bingling-sama <bingling_sama@qq.com>
2026-08-12 16:21:54 +0800

    ✨ feat(layout): update layout structure and remove banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
-	return <div>Home
-		<h1 className="text-3xl font-bold underline">Hello World</h1>
-		<a href="/guide">Guide</a>
-		<a href="/blog">Blog</a>
+	return <div className="h-full w-full">
+
 	</div>;
 }
