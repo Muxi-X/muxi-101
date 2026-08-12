3ac5a19f10ce7c941ef1d42cdf1f271c8ac0825f
bingling-sama <bingling_sama@qq.com>
2026-08-12 22:17:46 +0800

    🐳 chore(lint): remove eslint, config oxc toolchain

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-import type { NextConfig } from "next";
-import nextra from "nextra";
+import type { NextConfig } from 'next'
+import nextra from 'nextra'
 
-const withNextra = nextra({
-})
+const withNextra = nextra({})
 
 const nextConfig: NextConfig = withNextra({
-});
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+})
 
-export default nextConfig;
+export default nextConfig
